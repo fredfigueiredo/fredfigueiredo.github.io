2ce5a1dc785bfbdae5dd71f3cda4183d578f569b
fredfigueiredo <fef@changeyourflight.com>
2014-03-24 21:14:00 +0100

    Added grunt-gh-pages plugin.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
+  // project configuration
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
@@ -35,6 +35,16 @@ module.exports = function(grunt) {
       all: {
         path: 'http://localhost:<%= connect.server.options.port %>'
       }
+    },
+
+    // commits defined folder to specified branch
+    'gh-pages': {
+      options: {
+        base: 'public',
+        branch: 'master',
+        message: 'Auto-generated build commit'
+      },
+      src: ['**']
     }
 
   });
@@ -43,8 +53,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
   // Default task(s)
+  grunt.registerTask('publish', ['gh-pages']);
   grunt.registerTask('dev', ['connect', 'open', 'watch']);
   grunt.registerTask('default', ['watch']);
 
