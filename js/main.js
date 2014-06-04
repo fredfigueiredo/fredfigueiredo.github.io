4ce960d73aafb9765a0ee6e43e3c9bff799099e4
fredfigueiredo <fef@changeyourflight.com>
2014-06-04 10:29:16 +0200

    Auto-generated build commit.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-
+// Smooth scroll
+// Adapted from http://css-tricks.com/snippets/jquery/smooth-scrolling/
+$(function() {
+  $('a[href^="\\#"]').click(function() {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  });
+});
